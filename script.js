81923b5fc20322c460ad6c825e79f1d0f3c8ca4b
jonathan_uber <jonathanuber121207@gmail.com>
2026-02-19 20:02:38 -0300

    feat: implement scrollbar at user table

--- script.js
+++ script.js
@@ -155,9 +155,6 @@ function addUserAtList(name, email, id) {
                 <td class="px-4 text-sm py-2 font-inter text-secondary">${name}</td>
                 <td class="px-4 text-sm py-2 font-inter text-secondary">${email}</td>
                 <td class="px-4 py-2 text-red-600 font-medium font-inter flex flex-row">
-                    <button class="p-2 font-inter ">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#8A7650" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-pen-icon lucide-square-pen"><path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/></svg>
-                    </button>
                     <button class="p-2 font-inter flex flex-row gap-2" onclick="deleteUser('${id}')">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#8A7650" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
                     </button>
